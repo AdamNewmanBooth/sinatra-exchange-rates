4d456d7f13e2e74bfc78784e1a4f3e7e88e34725
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-23 04:36:07 +0000

    Commit3

--- app.rb
+++ app.rb
@@ -14,22 +14,38 @@ get("/") do
   @raw_response = HTTP.get(api_url)
 
   # Get the body of the response as a string
-  @raw_string = @raw_response.to_s
+  @string_response = @raw_response.to_s
 
   # Convert the string to JSON
-  @parsed_data = JSON.parse(@raw_string)
+  @parsed_response = JSON.parse(@string_response)
 
+  @currencies = @parsed_response.fetch("currencies")
   # Render a view template
   erb(:homepage)
-
-  # In the view template, embed the @parsed_data variable to
-  # see what you're working with.
-  
-  # From there, use your Hash/Array skills to make the homepage 
-  # match the target.
-  
-  # Remember to Make The Invisible Visible — View Source in 
-  # Chrome to see what your templates are actually outputting, 
-  # and embed as many instance variables as you need to (this 
-  # is the new equivalent of pretty-printing everything).
 end
+
+  get("/:from_currency") do
+      @the_symbol = params.fetch("first_symbol")
+
+      # Assemble the API url, including the API key in the query string
+      api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+      # Use HTTP.get to retrieve the API data
+      @raw_response = HTTP.get(api_url)
+
+      # Get the body of the response as a string
+      @string_response = @raw_response.to_s
+
+      # Convert the string to JSON
+      @parsed_response = JSON.parse(@string_response)
+
+      @currencies = @parsed_response.fetch("currencies")
+
+    erb(:step_one)
+
+end
+
+get("/:from_currency/to_currency")
+@original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
